56a91fa2e893a51fc7610a4cd11bcaf3c0ea3455
beginal <beginal01@gmail.com>
2020-04-16 14:04:50 +0900

    기본설정

---
+++ front/src/pages/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+const Index = () => {
+  return (
+    <> 
+    <Link href="./About"><a>about</a></Link>
+    <div> 인덱스 페이지. </div>
+    </>
+  );
+};
+
+export default Index;
